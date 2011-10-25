757e0513de07e88384d46d92462181c013351fc7
petru.isfan <petru.isfan@gmail.com>
2011-10-25 19:50:29 +0000

    Added a new GUI for client.

--- Client/src/main/java/com/bursa/client/gui/MainFrame.java
+++ Client/src/main/java/com/bursa/client/gui/MainFrame.java
@@ -1,144 +1,134 @@
 package com.bursa.client.gui;
 
-import java.awt.BorderLayout;
-import java.awt.Component;
-import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JComboBox;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.JTextArea;
-import javax.swing.border.TitledBorder;
-import javax.swing.table.JTableHeader;
 
-public class MainFrame extends JFrame {
-	private static final long serialVersionUID = -5284406119994569295L;
+public class MainFrame extends javax.swing.JFrame {
+	private static final long serialVersionUID = -8587135515102271344L;
 
-	public static final int frame_width = 700;
-    public static final int frame_height = 500;
-	
-    public JTextArea status = null;
-    
-    
-	public MainFrame() {
-		this.configureFrame();
-		this.addComponents();
-	}
+	private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JComboBox jComboBox1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JTable jTable1;
+    private javax.swing.JTextPane jTextPane1;
+    private JMenuItem m1;
+    private JMenuItem m2;
 
-	private void configureFrame() {
-		this.setSize(frame_width, frame_height);
-        this.setTitle("Stock exchange client");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
+    public MainFrame() {
+        initComponents();
+    }
 
-	private void addComponents() {
-		JPanel panel = new JPanel();
-        panel.setLayout(new BorderLayout());
+    /** This method is called from within the constructor to
+     * initialize the form.
+     */
+    @SuppressWarnings("unchecked")
+    private void initComponents() {
+
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextPane1 = new javax.swing.JTextPane();
+        jLabel1 = new javax.swing.JLabel();
+        jComboBox1 = new javax.swing.JComboBox();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenu2 = new javax.swing.JMenu();
+        m1 = new JMenuItem("Connection");
+        m2 = new JMenuItem("About");
         
-        // menu bar
-        panel.add(this.menuBar(), BorderLayout.NORTH);
-        
-        // center panel
-        JPanel center = new JPanel();
-        center.setLayout(new BorderLayout());
-        center.add(this.status(), BorderLayout.NORTH);
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        // center center panel
-        JPanel center2 = new JPanel();
-        center2.setLayout(new FlowLayout(FlowLayout.LEFT));
-        center2.add(new JLabel("Display:"));
-        center2.add(this.dropDown());
-        
-        center.add(center2, BorderLayout.CENTER);
-        
-        //center.add(this.dataTable(),BorderLayout.SOUTH);
-        
-        panel.add(center, BorderLayout.CENTER);
-        
-        this.add(panel);
-	}
+        jTextPane1.setBorder(javax.swing.BorderFactory.createTitledBorder("Status"));
+        jTextPane1.setEditable(false);
+        jTextPane1.setText("Connected to: - ");
+        jScrollPane1.setViewportView(jTextPane1);
 
-	/**
-	 * @return a menu bar
-	 */
-	private Component menuBar() {
-		JMenuBar menuBar = new JMenuBar();
-        
-		// New menu
-		JMenu new_menu = new JMenu("New");
-		JMenuItem conn = new JMenuItem("Connection");
+        jLabel1.setText("Show:");
 
-		conn.addActionListener(new ActionListener() {
-			
-			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-			}
-		});
-		
-		new_menu.add(conn);
-		menuBar.add(new_menu);
-		
-		// Help Menu
-		JMenu help_menu = new JMenu("Help");
-		JMenuItem about = new JMenuItem("About");
-		
-		about.addActionListener(new ActionListener() {
-			
-			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-		});
-		
-		help_menu.add(about);
-		menuBar.add(help_menu);
-		
-		return menuBar;
-	}
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Buy", "Sell" }));
 
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Id", "Company", "Number", "Value"
+            }
+        ));
+        jScrollPane2.setViewportView(jTable1);
 
-	private JTextArea status() {
-		status = new JTextArea();
-		
-		TitledBorder border = new TitledBorder("Status");
-		status.setBorder(border);
-		
-		status.setEditable(false);
-		status.setText("Connection Status: ");
-		
-		return status;
-	}
+        jButton1.setText("Buy shares");
 
-	private JComboBox<String> dropDown() {
-		String[] items = {//"Transactions" ,
-				"For sale", "For buy"};
-		
-		JComboBox<String> combo = new JComboBox<String>(items);
-		
-		return combo;
-	}
-	
-	private JScrollPane dataTable() {
-		String[] columnNames = {"ID", "Company", "number", "value"};
-		Object[][] data = { {"", "", "", ""},
-				{"", "", "", ""},
-				{"", "", "", ""},
-				{"", "", "", ""},
-				{"", "", "", ""}
-		};
-		JTable table = new JTable(data, columnNames);
-		
-		JScrollPane scrollPane = new JScrollPane(table);
-		//table.setFillsViewportHeight(true);
-		
-		return scrollPane;
-	}
+        jButton2.setText("Refresh");
+
+        jButton3.setText("Sell shares");
+
+        jMenu1.setText("New");
+        jMenu1.add(m1);
+        jMenuBar1.add(jMenu1);
+
+        jMenu2.setText("Help");
+        jMenu2.add(m2);
+        jMenuBar1.add(jMenu2);
+
+        setJMenuBar(jMenuBar1);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 334, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(jButton3)
+                        .addGap(34, 34, 34)
+                        .addComponent(jButton1))
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(36, 36, 36)
+                        .addComponent(jButton2))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(22, 22, 22)
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(149, 149, 149))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(jButton3)
+                        .addComponent(jButton1))
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton2))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 192, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(50, Short.MAX_VALUE))
+        );
+
+        pack();
+    }
 }
